673c8f7f37aeaf8cb43b28056c0968bd48e26c14
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-08-29 19:25:05 -0700

    Working on expo
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+// Your other imports go here
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+  return <NavigationContainer>{/* Your app code */}</NavigationContainer>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
