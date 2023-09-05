0d668b05cd95c1de4544f186982f8d46ef6c3e1c
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-09-04 18:44:27 -0700

    Password Checker file no longer required
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- teachers-console/api/supabase.ts
+++ teachers-console/api/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const 
+const Pb24iLCJpYXQiOjE2OTM4NzQ2NjYsImV4cCI6MjAwOTQ1MDY2Nn0.nzbHw3XU4qVouLYJRW0yuVNt89qNESX4tV8m606dd_A'
 const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
 
 export default supabase
