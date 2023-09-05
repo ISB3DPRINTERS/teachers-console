7e5c2d945589bf57abfb888cd8f03d5cf92a14cb
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-09-04 17:40:07 -0700

    Building someting :\
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- teachers-console/App.tsx
+++ teachers-console/App.tsx
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Routes, Route } from "react-router-dom";
-import { LoginPage } from "./pages/Login";
-import { HomePage } from "./pages/Home";
-import "./styles.css";
+import {useState} from 'react';
+import React from 'react';
+import passchecker from './api/passchecker'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NoPage from './pages/NoPage';
+import Layout from './pages/Layout';
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Login />} />
+          <Route path="" element={<Blogs />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
