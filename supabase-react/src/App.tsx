cc82d50af277b6b3f8c251afa90fb1cf8b85f119
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-09-05 17:31:17 -0700

    Attempting vite
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

---
+++ supabase-react/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+import { supabase } from './api/supabaseclient'
+import Auth from './pages/auth'
+import Account from './pages/account'
+import React from 'react'
+
+function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+    </div>
+  )
+}
+
+export default App
