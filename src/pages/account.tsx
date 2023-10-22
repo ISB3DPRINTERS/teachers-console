87df57c67e0b3b82ac45bf2518ba27fab4ee0b68
Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>
2023-10-22 11:20:19 -0700

    mocked up admin xonsolr
    Signed-off-by: Ravinder Olivier Singh Dadiala <ravinder-Olivier@outlook.com>

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -2,6 +2,16 @@ import { FC } from "react";
 
 export const Account: FC<{ token: string }> = () => {
     return (
-        <p>You're logged in!</p>
+        <div>
+            <h1>Teachers' Console</h1>
+            <br></br>
+            <p>Welcome to the ISB3DPRINTERS teachers' console, from here you can find many administrator tools and commands</p>
+            <br></br>
+            <strong>Never give students access to this page</strong>
+            <br></br>
+            <button>Reset Passkeys</button>
+            <br></br>
+            <h3>Info Table</h3>
+        </div>
     );
 }
